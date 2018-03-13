395788213ed06cd6eee163b25f754fa22526417a
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-03-13 13:27:49 -0300

    Refatorando

--- 1020.c
+++ 1020.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ANO 365
+#define MES 30
 
 int main() {
 	int dias, anos, meses, d;
 	scanf("%d", &dias);
 
-	anos = dias / 365;
-	meses = ((dias % 365) / 30);
-	d = dias - (anos * 365) - (meses * 30);
+	anos = dias / ANO;
+	meses = ((dias % ANO) / MES);
+	d = dias - (anos * ANO) - (meses * MES);
 
 	printf("%d ano(s)\n", anos);
 	printf("%d mes(es)\n", meses);
