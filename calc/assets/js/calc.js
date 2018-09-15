e076b4718dbe9539b5eae37e5ce3350f2d6c2e4a
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-09-15 02:06:47 -0300

    Adaptações para calculadora

--- calc/assets/js/calc.js
+++ calc/assets/js/calc.js
@@ -1,1 +1,6 @@
-alert('hey you');
+function totalChurrasco() {
+    var adults = parseInt($('input[name="adultos"]').val());
+    var children = parseInt($('input[name="criancas"]').val());
+    var total = adults + children;
+    alert("churrasco pra " + total + " pessoas");
+}
