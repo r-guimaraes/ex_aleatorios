c6bd1d60bfc4090a975ffa586b4f633045e58fb5
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-09-16 00:42:44 -0300

    Refatora js barbecue calc

--- calc/assets/js/calc.js
+++ calc/assets/js/calc.js
@@ -1,6 +1,7 @@
 var Churrasco = new function () {
     this.adultos = 0;
     this.criancas = 0;
+    this.custo_total = 0;
     this.precos = {
         picanha: 45,
         contra_file: 35,
@@ -13,8 +14,19 @@ var Churrasco = new function () {
       adulto: 0.5,
       crianca: 0.2
     };
+
     this.total_kg = function () {
         return (this.adultos * this.qtd_por_pessoa.adulto) + (this.criancas * this.qtd_por_pessoa.crianca);
+    };
+
+    this.calcula_custo = function (cortes) {
+        var custo = 0;
+        $(cortes).each(function (i,v) {
+            custo += Churrasco.precos[v];
+        });
+        var quilos = Churrasco.total_kg();
+
+        return ((custo * quilos) / cortes.length);
     }
 };
 
@@ -24,24 +36,29 @@ function totalChurrasco() {
     Churrasco.adultos = parseInt($('input[name="adultos"]').val());
     Churrasco.criancas = parseInt($('input[name="criancas"]').val());
 
-    var escolhas = [];
-    $(':checkbox:checked').each(function(i){
-        escolhas[i] = $(this).val();
-    });
-
-    var custo = 0;
-    $(escolhas).each(function (i,v) {
-        custo += Churrasco.precos[v];
-    });
+    var escolhas = pegarCortes();
+    var _preco = Churrasco.calcula_custo(escolhas);
+    var kg = Churrasco.total_kg();
 
     $(".calc-wrapper").removeClass('d-none').addClass('result');
-
-    var quilos = Churrasco.total_kg();
-    var qtd = "<hr> Quantidade necessária: " + quilos  + " Kgs";
-    var preco = "<br> Preço médio: R$ " + ((custo * quilos) / escolhas.length) + "<br><br>";
-    var obs = "<small><i>Considerando quantidades similares de cada corte selecionado</i></small>";
-
-    $("#valor").html( qtd + preco + obs);
+    $("#valor").html(retorno(kg, _preco));
 
     return false;
+}
+
+function retorno(kg, preco) {
+    var r = "<hr> Quantidade recomendada: " + kg  + " Kgs";
+    r += "<br> Preço médio: R$ " + preco + "<br><br>";
+    r += "<small><i>Considerando quantidades similares de cada corte selecionado</i></small>";
+
+    return r;
+}
+
+function pegarCortes() {
+    var cortes = [];
+    $(':checkbox:checked').each(function(i){
+        cortes[i] = $(this).val();
+    });
+
+    return cortes;
 }
