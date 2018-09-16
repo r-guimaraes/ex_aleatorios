fe3775dc8d17f36901f199e1f130a1e9ba4c62d0
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-09-15 23:18:20 -0300

    Ajustes de UX

--- calc/assets/js/calc.js
+++ calc/assets/js/calc.js
@@ -9,18 +9,14 @@ var Churrasco = new function () {
         coracao: 14,
         linguica: 12
     };
-
-    // em Kg
-    this.qtd_por_pessoa = {
+    this.qtd_por_pessoa = { // em Kg
       adulto: 0.5,
       crianca: 0.2
     };
     this.total_kg = function () {
         return (this.adultos * this.qtd_por_pessoa.adulto) + (this.criancas * this.qtd_por_pessoa.crianca);
     }
-
 };
-
 
 function totalChurrasco() {
     event.preventDefault();
@@ -38,11 +34,11 @@ function totalChurrasco() {
         custo += Churrasco.precos[v];
     });
 
-    $(".calc-wrapper").removeClass('d-none');
+    $(".calc-wrapper").removeClass('d-none').addClass('result');
 
     var quilos = Churrasco.total_kg();
-    var qtd = "Quantidade necessária: " + quilos  + " Kgs";
-    var preco = "<br> R$ " + ((custo * quilos) / escolhas.length) + "<br>";
+    var qtd = "<hr> Quantidade necessária: " + quilos  + " Kgs";
+    var preco = "<br> Preço médio: R$ " + ((custo * quilos) / escolhas.length) + "<br><br>";
     var obs = "<small><i>Considerando quantidades similares de cada corte selecionado</i></small>";
 
     $("#valor").html( qtd + preco + obs);
