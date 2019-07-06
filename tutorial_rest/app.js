d17412f8f6f1024c6cf6846aec7e639364e14608
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-07-05 23:25:51 -0300

    Improves file path handling

--- tutorial_rest/app.js
+++ tutorial_rest/app.js
@@ -3,53 +3,56 @@ var bodyParser = require('body-parser');
 var app = express();
 var fs = require("fs");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const file_path = __dirname + "/" + "users.json";
+
 app.get('/listUsers', function (req, res) {
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    fs.readFile(file_path, 'utf8', function (err, data) {
         res.json(JSON.parse(data));
         res.end(data);
     });
-})
+});
 
 app.post('/addUser', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
 
-        console.log( req.body)
+    console.log(req.query);
+
+    fs.readFile(file_path, 'utf8', function (err, data) {
         data = JSON.parse(data);
         // data["user4"] = JSON.parse(req.body);
         data["user4"] = req.body;
-        console.log( req.body );
-        // console.log( data );
+        console.log( req.body.name );
+        console.log( req.body.password );
+
         res.end( JSON.stringify(data));
     });
-})
+});
 
-app.get('/:id', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+app.get('/user/:id', function (req, res) {
+
+    fs.readFile(file_path, 'utf8', function (err, data) {
         data = JSON.parse( data );
         var user = data["user" + req.params.id]
         console.log( user );
         res.end( JSON.stringify(user));
     });
-})
+});
 
 app.delete('/deleteUser/:id', function (req, res) {
-    // First read existing users.
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+    fs.readFile(file_path, 'utf8', function (err, data) {
         data = JSON.parse( data );
         delete data["user" + req.params.id];
 
         console.log( data );
         res.end( JSON.stringify(data));
     });
-})
+});
 
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("REST tutorial app listening at http://%s:%s", host, port)
 });
