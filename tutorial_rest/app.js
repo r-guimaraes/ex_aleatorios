199bb85321d86d4a831f40f6e87b85e29cdfb72e
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-07-05 22:29:35 -0300

    loads list users as json

--- tutorial_rest/app.js
+++ tutorial_rest/app.js
@@ -3,29 +3,21 @@ var bodyParser = require('body-parser');
 var app = express();
 var fs = require("fs");
 
-/* var user = {
-   "user4" : {
-      "name" : "mohit",
-      "password" : "password4",
-      "profession" : "teacher",
-      "id": 4
-   }
-}
-*/
-
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json());
 
 app.get('/listUsers', function (req, res) {
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
+        res.json(JSON.parse(data));
+        res.end(data);
     });
 })
 
-app.put('/addUser', function (req, res) {
+app.post('/addUser', function (req, res) {
     // First read existing users.
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
+
+        console.log( req.body)
+        data = JSON.parse(data);
         // data["user4"] = JSON.parse(req.body);
         data["user4"] = req.body;
         console.log( req.body );
@@ -56,8 +48,8 @@ app.delete('/deleteUser/:id', function (req, res) {
 })
 
 var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
 
     console.log("Example app listening at http://%s:%s", host, port)
-})
+});
