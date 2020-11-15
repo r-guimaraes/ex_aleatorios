16382f7aa6d0b1d2fd09a02d600c810ca4fed7ee
Rodrigo <rodrigodeoliveira@hotmail.com.br>
2020-11-14 23:07:52 -0300

    Create serial.c

---
+++ C/processos/serial.c
@@ -0,0 +1,36 @@
+/* primos.c – identifica todos os numeros primos ate um certo valor*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int verifica_se_primo(long int);
+int main(int argc, char *argv[])
+{
+    long int numero = 0;
+    short int result, imprimir;
+    if (argc != 3)
+    {
+        printf("Uso correto: %s <numero> <imprimir=1,nao_imprimir=0>\n\n", argv[0]);
+        return 1;
+    }
+    numero = atol(argv[1]);
+    imprimir = atoi(argv[2]);
+    for (long int num_int = 1; num_int <= numero; num_int++)
+    {
+        result = verifica_se_primo(num_int);
+        if (imprimir == 1)
+            if (result == 1)
+                printf("%ld eh primo.\n", num_int);
+    }
+    return 0;
+}
+int verifica_se_primo(long int numero)
+{
+    long int ant;
+    for (ant = 2; ant <= (long int)sqrt(numero); ant++)
+    {
+        if (numero % ant == 0)
+            return 0;
+    }
+    if (ant * ant >= numero)
+        return 1;
+}
